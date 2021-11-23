8c98481b77197db203a433f1343c3df4938d592d
VISHNU D <91310267+VISHNUD06@users.noreply.github.com>
2021-11-23 21:38:48 +0530

    Add files via upload

---
+++ Reverse a word.py
@@ -0,0 +1,6 @@
+a=input('Enter any word:')
+k=len(a)-1
+Reverse=''
+for i in range(k,-1,-1):
+    Reverse=Reverse+(a[i])
+print (Reverse)
---
+++ pattern.py
@@ -0,0 +1,8 @@
+for i in range(0,10):
+    for j in range(0,i):
+       print('*',end='')
+    print()
+for k in range(8,0,-1):
+    for l in range(k):
+       print('*',end='')
+    print()
